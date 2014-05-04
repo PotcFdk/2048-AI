d3ad082e36e2c7c716a3e4153f9a8e597419cc7b
PotcFdk <PotcFdk@gmx.net>
2014-05-04 17:59:15 +0200

    Use the closest power of 2 less than the tile value for the color.

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -47,7 +47,10 @@ HTMLActuator.prototype.addTile = function (tile) {
   positionClass = this.positionClass(position);
 
   // We can't use classlist because it somehow glitches when replacing classes
-  var classes = ["tile", "tile-" + tile.value, positionClass];
+  var basevalue = 2;
+  while (basevalue < tile.value/2)
+	basevalue *= 2;
+  var classes = ["tile", "tile-" + basevalue, positionClass];
   this.applyClasses(element, classes);
 
   element.textContent = tile.value;
