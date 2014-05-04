dc86f4c1e54f2d9335f1903f3204dec21b224e46
PotcFdk <PotcFdk@gmx.net>
2014-05-04 17:35:15 +0200

    Initial simplifications.

--- js/grid.js
+++ js/grid.js
@@ -120,7 +120,7 @@ Grid.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 Grid.prototype.addRandomTile = function () {
   if (this.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.5 ? 1 : 2;
     //var value = Math.random() < 0.9 ? 256 : 512;
     var tile = new Tile(this.randomAvailableCell(), value);
 
@@ -190,8 +190,8 @@ Grid.prototype.move = function (direction) {
         var next      = self.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+        if (next && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value + next.value);
           merged.mergedFrom = [tile, next];
 
           self.insertTile(merged);
@@ -278,33 +278,7 @@ Grid.prototype.movesAvailable = function () {
 // Check for available matches between tiles (more expensive check)
 // returns the number of matches
 Grid.prototype.tileMatchesAvailable = function () {
-  var self = this;
-
-  //var matches = 0;
-
-  var tile;
-
-  for (var x = 0; x < this.size; x++) {
-    for (var y = 0; y < this.size; y++) {
-      tile = this.cellContent({ x: x, y: y });
-
-      if (tile) {
-        for (var direction = 0; direction < 4; direction++) {
-          var vector = self.getVector(direction);
-          var cell   = { x: x + vector.x, y: y + vector.y };
-
-          var other  = self.cellContent(cell);
-
-          if (other && other.value === tile.value) {
-            return true; //matches++; // These two tiles can be merged
-          }
-        }
-      }
-    }
-  }
-
-  //console.log(matches);
-  return false; //matches;
+  return true;
 };
 
 Grid.prototype.positionsEqual = function (first, second) {
